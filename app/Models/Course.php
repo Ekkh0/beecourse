5616221bbdb74d3036540233ea37acbf8d003ce2
JonathaIvan <jovanganteng12345@gmail.com>
2023-11-21 22:25:53 +0700

    Migrations And Model

---
+++ app/Models/Course.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    public function content() {
+        return $this->hasMany(Content::class);
+    }
+
+    public function usercourse() {
+        return $this->hasMany(UserCourse::class);
+    }
+
+    public function transaction(){
+        return $this->hasMany(Transaction::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function transaction() {
+        return $this->hasMany(Transaction::class);
+    }
+    public function usercourse() {
+        return $this->hasMany(UserCourse::class);
+    }
 }
