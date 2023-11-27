c20fd9339f55546a2b7fc7d19d01dbec151e6ebb
Dharmawan Ruslan <darmawanruslan12@gmail.com>
2023-11-27 19:19:37 +0700

    Fix bug about page + ubah jumlah item homepage

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -8,7 +8,7 @@ use App\Models\Course;
 class CourseController extends Controller
 {
     public function index(){
-        $courses = Course::paginate(2);
+        $courses = Course::paginate(3);
         return view("home", ["courses"=>$courses]);
     }
 
